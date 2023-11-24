f97047d781697de9170bb651da78e9e44b222b65
strikerlulu <strikerlulu7@gmail.com>
2023-11-24 16:01:49 +0530

    #

--- client/src/pages/Dashboard.vue
+++ client/src/pages/Dashboard.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
       <h2>This is Dashboard</h2>
-      
+      <Demo />
       <router-link to="/">Log out</router-link>
     </div>
   </template>
   
   <script>
+  import Demo from '../components/Dashboard/Demo.vue';
   export default {
     name: 'Dashboard',
+    components: {
+    Demo,
+  },
   };
   </script>
   
