897d7278bcac5f117210c531110d09c9697cb09b
Muhammed-Rashid-07 <rashid24601@gmail.com>
2023-11-25 07:33:34 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -70,7 +70,6 @@ app.post('/api/register', async (req, res) => {
 app.post('/api/login', async (req, res) => {
   try {
     const { email, password: providedPassword } = req.body; // Rename 'password' to 'providedPassword'
-    username = req.body.email;
 
     if (!email || !providedPassword) {
       return res.json({ status: 100, message: 'All fields must be filled' });
@@ -84,7 +83,15 @@ app.post('/api/login', async (req, res) => {
       const isPasswordMatch = await bcrypt.compare(providedPassword, hashedPassword);
 
       if (isPasswordMatch) {
-        return res.json({ status: 1, message: 'Login successful', userId: rows[0].id });
+        // Store user ID in local storage
+        const userId = rows[0].id;
+
+        // Example of storing user ID in local storage
+        // Note: This should be done on the client side (e.g., in your React or Angular app)
+        // You can use JavaScript to set an item in local storage
+        localStorage.setItem('userId', userId);
+
+        return res.json({ status: 1, message: 'Login successful', userId });
       } else {
         return res.json({ status: 0, message: 'Invalid credentials' });
       }
@@ -96,6 +103,7 @@ app.post('/api/login', async (req, res) => {
     return res.status(500).json({ status: 0, message: 'Internal Server Error' });
   }
 });
+
 
 //get all signups
 app.get('/api/signups', async (req, res) => {
@@ -113,3 +121,58 @@ const PORT = 9000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+
+//create all tasks
+app.get('/api/create-task', async (req, res) => {
+  try {
+    const {taskName, startDate, endDate, taskType, priority, description} = req.body;
+
+    const selectAllQuery = 'SELECT * FROM tasks';
+    const [rows] = await db.promise().execute(selectAllQuery);
+    return res.json({ status: 200, message: 'Success', data: rows });
+  } catch (err) {
+    console.error('Error:', err);
+    return res.status(500).json({ status: 0, message: 'Internal Server Error' });
+  }
+}
+)
+
+app.post('/api/create-task', async (req, res) => {
+    try {
+      const { userId, title, startDate, endDate, taskType, priority, description, selectedProject } = req.body;
+
+      if (!title || !startDate || !endDate) {
+        return res.json({ status: 0, message: 'All fields must be filled' });
+      } else if (title.length < 5 || title.length > 15) {
+        return res.json({ status: 0, message: 'Task title must contain between 5 and 15 characters' });
+      }
+
+      const checkTaskQuery = 'SELECT user_id FROM event WHERE user_id = ? AND task_name = ?';
+      const [taskRows] = await db.promise().execute(checkTaskQuery, [userId, title]);
+
+      if (taskRows.length > 0) {
+        return res.json({ status: 0, message: 'Task with the same title already exists for this user' });
+      }
+      const priorityMap = {
+        'High': 1,
+        'Medium': 2,
+        'Low': 3
+      };
+      
+      const priorityValue = priorityMap[priority];
+      const insertTaskQuery = `INSERT INTO event(user_id, task_name, start_date, end_date, task_type, priority, description, task_progress, task_done, project_id, created_at, updated_at)VALUES(?, ?, ?, ?, ?, ?, ?, 0, 0, ?, NOW(), NOW())`;
+
+      const [results] = await db.promise().execute(insertTaskQuery, [userId, title, startDate, endDate, taskType, priorityValue, description, selectedProject || null]);
+
+      if (results && results.insertId) {
+        return res.json({ status: 1, message: 'Task created successfully' });
+      } else {
+        return res.json({ status: 0, message: 'Failed to create the task' });
+      }
+    } catch (err) {
+      console.error('Error:', err);
+      return res.status(500).json({ status: 0, message: 'Internal Server Error' });
+    }
+  });
